bed2c57aeadd7d58ea54b04e58168fc3774238a8
Rook Soto <rooksoto@gmail.com>
2025-11-16 19:04:55 -0500

    Main Prints Summaries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod kyu;
+mod problem;
+
+use crate::problem::Problem;
+
 fn main() {
-    println!("Hello, Codewars!");
+    println!("         Codewars Rust          ");
+    println!("_____________SOLVED_____________");
+}
+
+fn print_problem_solved<P: Problem>(problem: P) {
+    println!("✅ \"{}\", Kyu {}", problem.problem_name(), problem.kyu());
 }
